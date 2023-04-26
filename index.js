c4f07eb3f7f18368456446f9f1d0357553f1681a
Anshhh1412 <ansh7080@gmail.com>
2023-04-27 00:06:23 +0530

    route for post requests add and updated ddl for some tables

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 const express = require('express');
-
 const app = express();
 const routes = require('./routes/route.js');
+var httpServer = require('http').Server(app);
+var socketServer = require('socket.io')(httpServer);
+
 
 require('dotenv').config();
 
 
 app.use(express.json());
 app.use('/api', routes);
+app.use('/images',express.static('images/gif/'));
 
-app.listen(3000, () => {
-    console.log(`Server Started at ${3000}`)
-})
+ip = "192.168.0.104"
+port = 3000
+
+// app.listen(3000, () => {
+//     console.log(`Server Started at ${3000}`)
+// })
+
+socketServer.on('connection', function(socket){
+    console.log("A Client has connected.");
+    socket.on('disconnect', function(){
+        console.log("A Client has disconnected.");
+    });
+});
+
+httpServer.listen(port, ip, function(){
+    console.log("Listening to "+ip+":"+port);
+});
--- routes/route.js
+++ routes/route.js
@@ -32,6 +32,27 @@ router.get("/user/:user_id", (req, res) => {
     });
 });
 
+// router.post for inserting values into user details table
+
+router.post("/signup", (req, res) => {
+   
+    const userID = req.body.id;
+    const response = {
+        status: "success",
+    }
+    console.log(req.body)
+    db.query('INSERT INTO user_details (userID) VALUES ($1)', [userID], (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        else{
+            res.send(response);
+        }
+    });
+});
+
+
+
 // /api/post/user_details/:user_id:
 // 	Returns {Post_id, post_image, post_description,Date_time,count_of_likes}
 
@@ -44,6 +65,50 @@ router.get("/post/user_details/:user_id", (req, res) => {
         res.send(result.rows);
     });
 });
+
+// /api/user/post/
+// Add post to the database
+
+router.post("/user/post", (req, res) => {
+
+    const userID = req.body.userID;
+    const postDescr = req.body.postDescr;
+    const postDate = new Date();
+
+    db.query('INSERT INTO post (userID,postDescr,postDate) VALUES ($1, $2, $3)', [userID,postDescr,postDate], (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        else{
+            // send the postID of the post inserted as response
+            db.query('SELECT postID FROM post WHERE userID = $1 AND postDescr = $2 AND postDate = $3', [userID,postDescr,postDate], (err, result) => {
+                if (err) {
+                    console.log(err);
+                }
+                res.send(result.rows);
+            });
+        }
+    });
+});
+
+
+// /api/post/like/
+// Add like to the database
+
+router.post("user/post/like", (req, res) => {
+    const postID = req.body.postID;
+    const userID = req.body.userID;
+
+    db.query('INSERT INTO likes (postID,userID) VALUES ($1, $2)', [postID,userID], (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        else{
+            res.send('like inserted successfully');
+        }
+    });
+});
+
 
 // /api/post/comments/:post_id:
 // 	returns {Comment_desc, comment_date, comment_id, user_id}
@@ -58,6 +123,26 @@ router.get("/post/comments/:post_id", (req, res) => {
     });
 });
 
+// api/user/post/comment/
+// Add comment to the database
+
+router.post("/user/post/comment", (req, res) => {
+    const postID = req.body.postID;
+    const userID = req.body.userID;
+    const commentDescr = req.body.commentDescr;
+    const commentDate = new Date();
+
+    db.query('INSERT INTO comment (postID,userID,commentDescr,commentDate) VALUES ($1, $2, $3, $4)', [postID,userID,commentDescr,commentDate], (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        else{
+            res.send('comment inserted successfully');
+        }
+    });
+});
+
+
 // /api/subscription/:user_id:
 // 	Subscription_title, subscription_desc, category, price, count_of_userid, list_of_uid,
 
@@ -69,6 +154,68 @@ router.get("/subscription/:user_id", (req, res) => {
         }
         res.send(result.rows);
     });
+});
+
+// /api/user/subscription/
+// 	Add subscription to the database
+
+router.post("/user/subscription", (req, res) => {
+    
+   // const subscriptionID = req.body.subscriptionID;
+    const mentorID = req.body.userID;
+    const subscriptionTitle = req.body.Title;
+    const subscriptionDesc = req.body.subsDescr;
+    const category = req.body.catagory;
+    const price = req.body.price;
+
+    db.query('INSERT INTO subscription (mentorID,title,subsDescr,catagory,price) VALUES ($1, $2, $3, $4, $5)', [mentorID,subscriptionTitle,subscriptionDesc,category,price], (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        else{
+            res.send('subscription inserted successfully');
+        }
+    });
+    
+});
+
+// /api/user/subscription/insert
+// 	Add subscription taken by user to the database
+
+router.post("/user/subscription/insert", (req, res) => {
+        
+    const subscriptionID = req.body.subscriptionID;
+    const userID = req.body.userID;
+    var default_rating = 5;
+
+    db.query('INSERT INTO user_subscription (subscriptionID,userID,userRating) VALUES ($1, $2,$3)', [subscriptionID,userID,default_rating], (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        else{
+            // find mentorID from subscriptionID
+            console.log("subscribed");
+            db.query('SELECT mentorID FROM subscription WHERE subscriptionID = $1', [subscriptionID], (err, result) => {
+                if (err) {
+                    console.log(err);
+                }
+                else{
+                    const mentorID = result.rows[0].mentorID;
+                    // create conversation between mentor and user
+                    db.query('INSERT INTO conversation (mentorID,userID) VALUES ($1, $2)', [mentorID,userID], (err, result) => {
+                        if (err) {
+                            console.log(err);
+                        }
+                        else{
+                            res.send('conversation created successfully b/w',mentorID,'and',userID);
+                        }
+                    });
+                }
+            });
+            
+        }
+    });
+        
 });
 
 
@@ -131,6 +278,26 @@ router.get("/chat/conversation/:conversation_id", (req, res) => {
         }
         res.send(result.rows);
     });
+});
+
+// /api/chat/conversation/sendmessage : send message to the database
+// add message to the database
+
+router.post("/chat/conversation.sendmessage", (req, res) => {
+    const conversationID = req.body.conversationID;
+    const userID = req.body.userID;
+    const messageDesc = req.body.messageDesc;
+    const messageDate = new Date();
+
+    db.query('INSERT INTO message (conversationID,userID,messageDesc,messageDate) VALUES ($1, $2, $3, $4)', [conversationID,userID,messageDesc,messageDate], (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        else{
+            res.send('message inserted successfully');
+        }
+    });
+    
 });
 
 
